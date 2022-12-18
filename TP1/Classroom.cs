a3fea729b0c9def68d59cfda6289dfcea0a6d7ad
loelkhalid <lotfi.elkhalidy@gmail.com>
2022-12-18 17:28:46 +0100

    Added the first exercice

--- TP1/Classroom.cs
+++ TP1/Classroom.cs
@@ -1,7 +1,43 @@
-﻿class Classroom
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World !");
+﻿using System;
+
+class Classroom {
+    static void Main(string[] args) {
+        int NumberOfStudents = 0;
+        Classroom Isima = new Classroom();
+        List<Student> StudentList = new List<Student>();
+
+        Console.WriteLine("Welcome !");
+        Console.WriteLine("How many students are in this class ?");
+        NumberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+        StudentList = Isima.CreateStudents(NumberOfStudents);
+
+        foreach(Student s in StudentList) {
+            s.SetStudentInfo();
+        }
+
+        Console.WriteLine("Do you want to display the info on the students ? If yes, type 'true'.");
+        bool UserResponse = Convert.ToBoolean(Console.ReadLine());
+
+        if (UserResponse) {
+            int i = 0;
+            foreach (Student s in StudentList)
+            {
+                i++;
+                s.DisplayStudentInfo(i);
+            }
+        }
+
+    }
+
+    public List<Student> CreateStudents(int NbStudents) {
+        List<Student> StudentList = new List<Student>();
+
+        for (int i=0; i<NbStudents; i++) {
+            Student s = new Student();
+            StudentList.Add(s);
+        }
+
+        return StudentList;
     }
 }
--- TP1/Student.cs
+++ TP1/Student.cs
@@ -1,4 +1,36 @@
-﻿class Student
-{
+﻿using System;
 
+class Student {
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public int Promo;
+    public string BirthDate;
+
+    public void SetStudentInfo() {
+        Console.WriteLine("-------- Enter the following informations --------");
+
+        Console.Write("Student name : ");
+        this.Name = Console.ReadLine();
+        
+        Console.Write("Student surname : ");
+        this.Surname = Console.ReadLine();
+        
+        Console.Write("Promo : ");
+        this.Promo = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Student date of birth (DD-MM-YYYY) : ");
+        this.BirthDate = Console.ReadLine();
+
+        Console.WriteLine("--------------------------------------------------");
+    }
+
+    public void DisplayStudentInfo(int id) {
+        Console.WriteLine("------------------ Student " + id + " Info ------------------");
+        Console.WriteLine("Name : " + this.Name);
+        Console.WriteLine("Surname : " + this.Surname);
+        Console.WriteLine("Promo : " + this.Promo);
+        Console.WriteLine("Birth date (DD-MM-YYYY) : " + this.BirthDate);
+        Console.WriteLine("--------------------------------------------------");
+    }
 }
