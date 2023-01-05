21ce6d55b5e076b33967bd8af42d7c72336b9e08
Lotfi <lotfi.elkhalidy@gmail.com>
2023-01-05 17:48:18 +0100

    Added the Lecture class

--- TP1/Classroom.cs
+++ TP1/Classroom.cs
@@ -3,48 +3,30 @@ using System.Linq;
 
 class Classroom {
     static void Main(string[] args) {
-        int NumberOfStudents = 0;
+        int Choice = 0;
         Classroom Isima = new Classroom();
-        List<Student> StudentList = new List<Student>();
 
         Console.WriteLine("Welcome !");
-        Console.WriteLine("How many students are in this class ?");
-        NumberOfStudents = Convert.ToInt32(Console.ReadLine());
 
-        StudentList = Isima.CreateStudents(NumberOfStudents);
+        while (true) {
+            Console.WriteLine("----------------------- MENU ---------------------");
+            Console.WriteLine("What do you want to do ? (Enter a number)");
+            Console.WriteLine("        1. Manage students");
+            Console.WriteLine("        2. Manage lectures");
+        
+            Choice = Convert.ToInt32(Console.ReadLine());
 
-        foreach(Student s in StudentList) {
-            s.SetStudentInfo();
-        }
-
-        Console.WriteLine("Do you want to display the info on the students ? If yes, type 'true'.");
-        bool UserResponse = Convert.ToBoolean(Console.ReadLine());
-
-        if (UserResponse) {
-            int i = 0;
-
-            // Sort the students by their surname using LinQ
-            Console.WriteLine("How do you want to order the students according to their surnames ? type 'ASC' or 'DESC'.");
-            string UserOrderByChoice = Convert.ToString(Console.ReadLine());
-
-            List<string> SortedSurnames = new List<string>();
-            foreach (Student s in StudentList) {
-                SortedSurnames.Add(s.Surname);
-            }
-            SortedSurnames.Sort();
-
-            List<Student> SortedList = new List<Student>();
-            if (UserOrderByChoice == "ASC")
-                 SortedList = StudentList.OrderBy(s => SortedSurnames.IndexOf(s.Surname)).ToList();
-            else if (UserOrderByChoice == "DESC")
-                SortedList = StudentList.OrderByDescending(s => SortedSurnames.IndexOf(s.Surname)).ToList();
-
-            foreach (Student s in SortedList) {
-                i++;
-                s.DisplayStudentInfo(i);
+            switch (Choice) {
+                case 1:
+                    Isima.manageStudents();
+                    break;
+                case 2:
+                    Isima.manageLectures();
+                    break;
+                default:
+                    break;
             }
         }
-
     }
 
     public List<Student> CreateStudents(int NbStudents) {
@@ -56,5 +38,71 @@ class Classroom {
         }
 
         return StudentList;
+    }
+     
+    public List<Student> SortStudents(string type, List<Student> StudentList) {
+        List<string> SortedSurnames = new List<string>();
+        List<Student> SortedList = new List<Student>();
+
+        foreach (Student s in StudentList)
+        {
+            SortedSurnames.Add(s.Surname);
+        }
+        SortedSurnames.Sort();
+
+        if (type == "ASC")
+            SortedList = StudentList.OrderBy(s => SortedSurnames.IndexOf(s.Surname)).ToList();
+        else if (type == "DESC")
+            SortedList = StudentList.OrderByDescending(s => SortedSurnames.IndexOf(s.Surname)).ToList();
+
+        return SortedList;
+    }
+
+    public void manageStudents() {
+        int NumberOfStudents = 0;
+        List<Student> StudentList = new List<Student>();
+        List<Student> SortedList = new List<Student>();
+
+        Console.WriteLine("How many students are in this class ?");
+        NumberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+        StudentList = CreateStudents(NumberOfStudents);
+
+        foreach (Student s in StudentList)
+        {
+            s.SetStudentInfo();
+        }
+
+        Console.WriteLine("Do you want to display the info on the students ? If yes, type 'true'.");
+        bool UserResponse = Convert.ToBoolean(Console.ReadLine());
+
+        if (UserResponse)
+        {
+            int i = 0;
+
+            // Sort the students by their surname using LinQ
+            Console.WriteLine("How do you want to order the students according to their surnames ? type 'ASC' or 'DESC'.");
+            string UserOrderByChoice = Convert.ToString(Console.ReadLine());
+
+            SortedList = SortStudents(UserOrderByChoice, StudentList);
+
+            foreach (Student s in SortedList)
+            {
+                i++;
+                s.DisplayStudentInfo(i);
+            }
+        }
+    }
+
+    public void manageLectures() {
+        int NumberOfLectures = 0;
+
+        Console.WriteLine("How many lectures do u want to add ?");
+        NumberOfLectures = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("bla bla bla");
+
+
+        List<Lecture> LectureList = new List<Lecture>();
     }
 }
---
+++ TP1/Lecture.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Lecture {
+    public string Name { get; set; }
+    public List<string> Professors { get; set; }
+    public List<string> Classrooms { get; set; }
+    public List<Student> StudentList { get; set; }
+
+    public void SetLectureInfo() {
+        int NbProfessors = 0;
+        int NbClassrooms = 0;
+
+        Console.WriteLine("-------- Enter the following informations --------");
+
+        Console.Write("Lecture name : ");
+        this.Name = Console.ReadLine();
+
+        Console.Write("How many profesors give this lecture ?");
+        NbProfessors = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < NbProfessors; i++) {
+            Console.Write("Professor name : ");
+            this.Professors[i] = Convert.ToString(Console.ReadLine());
+        }
+
+        Console.Write("How many classrooms ensure this lecture ?");
+        NbClassrooms = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < NbClassrooms; i++) {
+            Console.Write("Classroom name : ");
+            this.Classrooms[i] = Convert.ToString(Console.ReadLine());
+        }
+
+        Console.WriteLine("--------------------------------------------------");
+    }
+
+    public void DisplayLectureInfo(int id) {
+        int i = 0;
+
+        Console.WriteLine("------------------ Lecture " + id + " Info ------------------");
+        Console.WriteLine("Name : " + this.Name);
+
+        foreach(string professor in this.Professors) {
+            Console.WriteLine("Professor :" + professor);
+        }
+
+        foreach (string classroom in this.Classrooms) {
+            Console.WriteLine("Classroom :" + classroom);
+        }
+
+        foreach (Student s in this.StudentList) {
+            i++;
+            s.DisplayStudentInfo(i);
+        }
+
+        Console.WriteLine("--------------------------------------------------");
+    }
+}
